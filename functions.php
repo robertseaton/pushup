e9ca3b8e6d138f0f55f9e110fa98f817d1e462b5
robert seaton <robbpseaton@gmail.com>
2014-07-27 16:59:41 -0500

    Remove header, add below post widget section.

--- functions.php
+++ functions.php
@@ -14,4 +14,21 @@ function sp_custom_footer() {
           <p>&copy; Copyright 2012 <a href="http://mydomain.com/">My Domain</a> &middot; All Rights Reserved &middot; Powered by <a href="http://wordpress.org/">WordPress</a> &middot; <a href="http://mydomain.com/wp-admin">Admin</a></p>
      -->
 <?php
-}
+                                                                                                                                                                                              }
+/* This is the below post widget area. */
+
+genesis_register_sidebar( array(
+'id' => 'below-post',
+'name' => __( 'Below Post', 'genesis' ),
+'description' => __( 'This is the below post widget.', 'themename' ),
+) );
+
+add_action( 'genesis_entry_footer', 'below_post_text' );
+
+function below_post_text() {
+	if (is_single()) {
+		genesis_widget_area('below-post', array('before' => '<div class="below-post widget-area">',));
+        }}
+
+/* This removes the header. */
+remove_action('genesis_header','genesis_do_header');
