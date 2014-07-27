ed48582dad4045d93afa55c3d48e3d4d3ccefa9f
robert seaton <robbpseaton@gmail.com>
2014-07-27 16:45:19 -0500

    remove footer

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@ require_once( get_template_directory() . '/lib/init.php' );
 add_theme_support( 'html5' );
 add_theme_support( 'genesis-responsive-viewport' );
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+add_action( 'genesis_footer', 'sp_custom_footer' );
+function sp_custom_footer() {
+     ?>
+     <!--
+          <p>&copy; Copyright 2012 <a href="http://mydomain.com/">My Domain</a> &middot; All Rights Reserved &middot; Powered by <a href="http://wordpress.org/">WordPress</a> &middot; <a href="http://mydomain.com/wp-admin">Admin</a></p>
+     -->
+<?php
+}
