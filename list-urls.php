3aa2352e3bb29a2448ee77a1df934cc05a548805
robert seaton <robbpseaton@gmail.com>
2014-09-02 21:31:50 -0500

    Attempt 2.

--- list-urls.php
+++ list-urls.php
@@ -1,6 +1,11 @@
+<?php
+/*
+Template Name: My Custom Page
+*/
+get_header(); ?>
 <h3>Last 50 Articles</h3>
  <ul>
-<?php query_posts('showposts=50'); ?>
+<?php query_posts('showposts=200'); ?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<li><?php the_permalink() ?></li>
 <?php endwhile; endif; ?>
