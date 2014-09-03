6be8a5ceedf132880c1f0b23f88737f8e8683d29
robert seaton <robbpseaton@gmail.com>
2014-09-02 21:34:57 -0500

    Take 3.

--- list-urls.php
+++ list-urls.php
@@ -1,12 +1,37 @@
-<?php
-/*
-Template Name: My Custom Page
-*/
-get_header(); ?>
-<h3>Last 50 Articles</h3>
- <ul>
-<?php query_posts('showposts=200'); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<li><?php the_permalink() ?></li>
-<?php endwhile; endif; ?>
- </ul>
+	
+
+    <?php
+     
+    include "wp-load.php";
+     
+    /*you have 2 options in the line below
+    staus= publish will return posts and pages with type, URL and title
+    status= any will return in addition any attachments such as images
+    in that post immediately after in the list with URL and file name  */
+    $posts = new WP_Query('post_type=any&posts_per_page=-1&post_status=publish');
+    $posts = $posts->posts;
+     
+     
+    header('Content-type:text/plain');
+    foreach($posts as $post) {
+        switch ($post->post_type) {
+            case 'revision':
+            case 'nav_menu_item':
+                break;
+            case 'page':
+                $permalink = get_page_link($post->ID);
+                break;
+            case 'post':
+                $permalink = get_permalink($post->ID);
+                break;
+            case 'attachment':
+                $permalink = get_attachment_link($post->ID);
+                break;
+            default:
+                $permalink = get_post_permalink($post->ID);
+                break;
+        }
+        echo "\n{$post->post_type}\t{$permalink}\t{$post->post_title}";
+    }
+    ?>
+
