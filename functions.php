fb9d8f6a8b1ba791f865687736864a620750f4b5
robert seaton <robbpseaton@gmail.com>
2014-09-13 13:50:47 -0500

    Try again.

--- functions.php
+++ functions.php
@@ -79,6 +79,6 @@ function crunchify_script_add_body() {
 /**      wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js', array( 'jquery' ), '4.0', false ); **/
 /**      wp_enqueue_script( 'jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', array( 'jquery' ), '1.10.3' ); **/
       wp_enqueue_script( 'add-this', 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e98cc990c4e19d6', array( 'addthis' ), '0.1' );
-      wp_enqueue_script( 'follow', get_stylesheet_directory_uri() . '/js/pushup.js', array( 'pushup' ), '', true );
-      wp_enqueue_script( 'bpopup', get_stylesheet_directory_uri() . '/js/jquery.bpopup.min.js', array( 'pushup' ), '', true );
+      wp_enqueue_script( 'follow', get_stylesheet_directory_uri() . '/js/pushup.js', array( ), '', true );
+      wp_enqueue_script( 'bpopup', get_stylesheet_directory_uri() . '/js/jquery.bpopup.min.js', array( ), '', true );
 }
