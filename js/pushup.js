3b5d9618c4878e49103233fbccccb81296135a33
robert seaton <robbpseaton@gmail.com>
2014-09-13 14:22:33 -0500

    Attempt to add bpopup code to pushup.js.

--- js/pushup.js
+++ js/pushup.js
@@ -7,3 +7,24 @@ jQuery(function( $ ){
     } );
 	
 });
+
+;(function($) {
+
+         // DOM Ready
+        $(function() {
+
+            // Binding a click event
+            // From jQuery v.1.7.0 use .on() instead of .bind()
+            $('#popup_button').bind('click', function(e) {
+
+                // Prevents the default action to be triggered. 
+                e.preventDefault();
+
+                // Triggering bPopup when click event is fired
+                $('#subscribe_popup').bPopup();
+
+            });
+
+        });
+
+})(jQuery);
